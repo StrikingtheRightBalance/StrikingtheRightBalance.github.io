4cbcbc1e4e5386499c205e1f6a47f81158ef6186
StrikingtheRightBalance <1372405178@qq.com>
2024-09-26 00:58:44 -0400

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,18 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
 	return (
 		<div>
-			<h3 className={styles.titleWrapper}>{"Time and Place"}</h3>
-			<p>
+			{/* <h3 className={styles.titleWrapper}>{"Time"}</h3>
 			Date: October 14, 2024
 			<p></p>
 			Time: 3:50-4:00 EST 
-			</p>
+			 */}
+			  <p style={{ textAlign: "left", marginTop: "20px" }}>
+                <strong>To discuss more on similar topics, please attend our panel discuss: <a href="https://vis-panel-enough-evaluation.github.io/
+" target="_blank" rel="noopener noreferrer">
+    (Yet Another) Evaluation Needed? A Panel Discussion on Evaluation Trends in Visualization
+  </a>
+				</strong>
+            </p>
 			<div className={styles.realFooter}>
 				<h6>
 				{"Copyright © 2024 Feng Lin"}
